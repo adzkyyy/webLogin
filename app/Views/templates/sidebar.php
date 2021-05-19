d3255feb3ef78b5f1d6ffa67bf5a347a0e9f946b
adzkyyy <adzkizaki24@gmail.com>
2021-05-19 09:04:11 +0700

    setengah_jalan

---
+++ app/Views/templates/sidebar.php
@@ -0,0 +1,55 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                <div class="sidebar-brand-icon rotate-n-15">
+                    <i class="fab fa-apple"></i>
+                </div>
+                <div class="sidebar-brand-text mx-3">berdistraksi</div>
+            </a>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+
+            <!-- Heading -->
+            <div class="sidebar-heading">
+                User Profile
+            </div>
+
+            <!-- Nav Item - Pages Collapse Menu -->
+
+            <!-- Nav Item - Charts -->
+            <li class="nav-item">
+                <a class="nav-link" href="charts.html">
+                    <i class="fas fa-user"></i>
+                    <span>My Profile</span></a>
+            </li>
+
+            <!-- Nav Item - Tables -->
+            <li class="nav-item">
+                <a class="nav-link" href="tables.html">
+                    <i class="fas fa-user-edit"></i>
+                    <span>Edit Profile</span></a>
+            </li>
+
+            <hr class="sidebar-divider">
+
+            <li class="nav-item">
+                <a class="nav-link" href="tables.html">
+                    <i class="fas fa-sign-out-alt"></i>
+                    <span>Logout</span></a>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider d-none d-md-block">
+
+            <!-- Sidebar Toggler (Sidebar) -->
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+            </div>
+
+        </ul>
---
+++ app/Views/user/index.php
@@ -0,0 +1,15 @@
+<?php
+
+use SebastianBergmann\Template\Template;
+?>
+<?= $this->extend('templates/index'); ?>
+
+<?= $this->section('page-content');?>
+
+<div class="container-fluid">
+        <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+
+</div>
+
+<?= $this->endSection();?>
